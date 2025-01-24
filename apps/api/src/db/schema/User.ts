8629c3cce7f76f2f8aa318561942b72456c9b27c
Shaun <shaunlimweimin@gmail.com>
2025-01-24 13:04:54 +0800

    feat: add User status

--- apps/api/src/db/schema/User.ts
+++ apps/api/src/db/schema/User.ts
@@ -1,7 +1,15 @@
-import { pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, varchar } from "drizzle-orm/pg-core";
+
+export const UserStatusEnum = pgEnum("status", [
+	"PENDING",
+	"ACTIVE",
+	"BANNED",
+	"DELETED",
+]);
 
 export const UsersTable = pgTable("Users", {
 	id: varchar().primaryKey().unique(), // clerk
 	collections: text().default("[]"),
 	username: varchar({ length: 20 }).unique(),
+	status: UserStatusEnum().default("ACTIVE"),
 });
